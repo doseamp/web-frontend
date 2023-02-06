4ac7c3fc1500a73cef08d32556ea6507102988d1
Xelem <anselmchizurum@gmail.com>
2023-02-06 11:52:30 +0100

    Completed signin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Home from "./pages/Home";
 import Signup from "./components/Signup";
+import Signin from "./components/Signin";
 
 function App() {
   return (
     <div className="App">
       <Home />
       <Signup />
+      <Signin />
     </div>
   );
 }
---
+++ src/components/Signin.jsx
@@ -0,0 +1,49 @@
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+
+const auth = getAuth();
+
+const Signin = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleClick = {
+    signin: async function (e) {
+      e.preventDefault();
+
+      try {
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const user = userCredential.user;
+        console.log(user);
+        return true;
+      } catch (error) {
+        console.error(error);
+        return { error: error.message };
+      }
+    },
+  };
+
+  return (
+    <div>
+      <h1>Sign in</h1>
+      <form onSubmit={handleClick.signin}>
+        <label>Email</label>
+        <input
+          type="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit">Sign in</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signin;
