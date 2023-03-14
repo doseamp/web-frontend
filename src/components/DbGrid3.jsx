e15c33d4e68d334440e81e36e47dc6dd5723fdab
Xelem <anselmchizurum@gmail.com>
2023-03-14 01:04:33 +0100

    Added the third section of the grid in the dashboard

---
+++ src/components/DbGrid3.jsx
@@ -0,0 +1,68 @@
+import { Box, Stack, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const DbGrid3 = () => {
+  return (
+    <Box sx={{ backgroundColor: "#ffffff", borderRadius: 2, px: 2, py: 2 }}>
+      <Box display="flex" alignItems="center" sx={{ mb: 2 }}>
+        <Typography sx={{ fontSize: "15px" }}>Recent transactions</Typography>
+        <Link
+          to=""
+          style={{
+            color: "#54adf3",
+            marginLeft: "auto",
+            fontSize: "8px",
+          }}
+        >
+          View all
+        </Link>
+      </Box>
+      <Stack direction="column">
+        <Stack direction="row" sx={{ py: 1 }}>
+          <Typography sx={{ fontWeight: "bold" }}>NGN ⇌ USDT</Typography>
+          <Box sx={{ ml: "auto" }}>
+            <Typography sx={{ color: "red" }}>-$378.33</Typography>
+            <Typography sx={{ fontSize: "8px" }}>Today, 10.40 am</Typography>
+          </Box>
+        </Stack>
+        <Stack direction="row" sx={{ py: 1 }}>
+          <Typography sx={{ fontWeight: "bold" }}>NGN ⇌ USDT</Typography>
+          <Box sx={{ ml: "auto" }}>
+            <Typography sx={{ color: "green" }}>+$378.33</Typography>
+            <Typography sx={{ fontSize: "8px" }}>Today, 10.40 am</Typography>
+          </Box>
+        </Stack>
+        <Stack direction="row" sx={{ py: 1 }}>
+          <Typography sx={{ fontWeight: "bold" }}>NGN ⇌ USDT</Typography>
+          <Box sx={{ ml: "auto" }}>
+            <Typography sx={{ color: "red" }}>-$378.33</Typography>
+            <Typography sx={{ fontSize: "8px" }}>Today, 10.40 am</Typography>
+          </Box>
+        </Stack>
+        <Stack direction="row" sx={{ py: 1 }}>
+          <Typography sx={{ fontWeight: "bold" }}>NGN ⇌ USDT</Typography>
+          <Box sx={{ ml: "auto" }}>
+            <Typography sx={{ color: "green" }}>+$378.33</Typography>
+            <Typography sx={{ fontSize: "8px" }}>Today, 10.40 am</Typography>
+          </Box>
+        </Stack>
+        <Stack direction="row" sx={{ py: 1 }}>
+          <Typography sx={{ fontWeight: "bold" }}>NGN ⇌ USDT</Typography>
+          <Box sx={{ ml: "auto" }}>
+            <Typography sx={{ color: "red" }}>-$378.33</Typography>
+            <Typography sx={{ fontSize: "8px" }}>Today, 10.40 am</Typography>
+          </Box>
+        </Stack>
+        <Stack direction="row" sx={{ py: 1 }}>
+          <Typography sx={{ fontWeight: "bold" }}>NGN ⇌ USDT</Typography>
+          <Box sx={{ ml: "auto" }}>
+            <Typography sx={{ color: "green" }}>+$378.33</Typography>
+            <Typography sx={{ fontSize: "8px" }}>Today, 10.40 am</Typography>
+          </Box>
+        </Stack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default DbGrid3;
