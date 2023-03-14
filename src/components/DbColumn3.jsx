ef7e256fd7b35fbd535322d17ca230a6b469490f
Xelem <anselmchizurum@gmail.com>
2023-03-15 00:50:01 +0100

    Completed third grid section

---
+++ src/components/DbColumn3.jsx
@@ -0,0 +1,224 @@
+import { Link } from "react-router-dom";
+import { Box, Card, CardContent, Typography, Stack } from "@mui/material";
+
+const DbColumn3 = () => {
+  return (
+    <Box>
+      <Card>
+        <CardContent>
+          <Box display="flex" alignItems="center" sx={{ mb: 2 }}>
+            <Typography sx={{ fontWeight: "bold" }}>
+              Recent transactions
+            </Typography>
+            <Link
+              to=""
+              style={{
+                color: "#54adf3",
+                marginLeft: "auto",
+                fontSize: "10px",
+              }}
+            >
+              View all
+            </Link>
+          </Box>
+          <Stack direction="column">
+            <Stack direction="row" sx={{ py: 1 }}>
+              <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+                NGN ⇌ USDT
+              </Typography>
+              <Box sx={{ ml: "auto" }}>
+                <Typography
+                  variant="body2"
+                  sx={{ fontWeight: "bold", color: "red" }}
+                >
+                  -$378.33
+                </Typography>
+                <Typography sx={{ fontSize: "8px" }}>
+                  Today, 10.40 am
+                </Typography>
+              </Box>
+            </Stack>
+            <Stack direction="row" sx={{ py: 1 }}>
+              <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+                NGN ⇌ USDT
+              </Typography>
+              <Box sx={{ ml: "auto" }}>
+                <Typography
+                  variant="body2"
+                  sx={{ fontWeight: "bold", color: "green" }}
+                >
+                  -$378.33
+                </Typography>
+                <Typography sx={{ fontSize: "8px" }}>
+                  Today, 10.40 am
+                </Typography>
+              </Box>
+            </Stack>
+            <Stack direction="row" sx={{ py: 1 }}>
+              <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+                NGN ⇌ USDT
+              </Typography>
+              <Box sx={{ ml: "auto" }}>
+                <Typography
+                  variant="body2"
+                  sx={{ fontWeight: "bold", color: "red" }}
+                >
+                  -$378.33
+                </Typography>
+                <Typography sx={{ fontSize: "8px" }}>
+                  Today, 10.40 am
+                </Typography>
+              </Box>
+            </Stack>
+            <Stack direction="row" sx={{ py: 1 }}>
+              <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+                NGN ⇌ USDT
+              </Typography>
+              <Box sx={{ ml: "auto" }}>
+                <Typography
+                  variant="body2"
+                  sx={{ fontWeight: "bold", color: "green" }}
+                >
+                  -$378.33
+                </Typography>
+                <Typography sx={{ fontSize: "8px" }}>
+                  Today, 10.40 am
+                </Typography>
+              </Box>
+            </Stack>
+            <Stack direction="row" sx={{ py: 1 }}>
+              <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+                NGN ⇌ USDT
+              </Typography>
+              <Box sx={{ ml: "auto" }}>
+                <Typography
+                  variant="body2"
+                  sx={{ fontWeight: "bold", color: "red" }}
+                >
+                  -$378.33
+                </Typography>
+                <Typography sx={{ fontSize: "8px" }}>
+                  Today, 10.40 am
+                </Typography>
+              </Box>
+            </Stack>
+            <Stack direction="row" sx={{ py: 1 }}>
+              <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+                NGN ⇌ USDT
+              </Typography>
+              <Box sx={{ ml: "auto" }}>
+                <Typography
+                  variant="body2"
+                  sx={{ fontWeight: "bold", color: "red" }}
+                >
+                  -$378.33
+                </Typography>
+                <Typography sx={{ fontSize: "8px" }}>
+                  Today, 10.40 am
+                </Typography>
+              </Box>
+            </Stack>
+            <Stack direction="row" sx={{ py: 1 }}>
+              <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+                NGN ⇌ USDT
+              </Typography>
+              <Box sx={{ ml: "auto" }}>
+                <Typography
+                  variant="body2"
+                  sx={{ fontWeight: "bold", color: "green" }}
+                >
+                  -$378.33
+                </Typography>
+                <Typography sx={{ fontSize: "8px" }}>
+                  Today, 10.40 am
+                </Typography>
+              </Box>
+            </Stack>
+            <Stack direction="row" sx={{ py: 1 }}>
+              <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+                NGN ⇌ USDT
+              </Typography>
+              <Box sx={{ ml: "auto" }}>
+                <Typography
+                  variant="body2"
+                  sx={{ fontWeight: "bold", color: "red" }}
+                >
+                  -$378.33
+                </Typography>
+                <Typography sx={{ fontSize: "8px" }}>
+                  Today, 10.40 am
+                </Typography>
+              </Box>
+            </Stack>
+            <Stack direction="row" sx={{ py: 1 }}>
+              <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+                NGN ⇌ USDT
+              </Typography>
+              <Box sx={{ ml: "auto" }}>
+                <Typography
+                  variant="body2"
+                  sx={{ fontWeight: "bold", color: "red" }}
+                >
+                  -$378.33
+                </Typography>
+                <Typography sx={{ fontSize: "8px" }}>
+                  Today, 10.40 am
+                </Typography>
+              </Box>
+            </Stack>
+            <Stack direction="row" sx={{ py: 1 }}>
+              <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+                NGN ⇌ USDT
+              </Typography>
+              <Box sx={{ ml: "auto" }}>
+                <Typography
+                  variant="body2"
+                  sx={{ fontWeight: "bold", color: "green" }}
+                >
+                  -$378.33
+                </Typography>
+                <Typography sx={{ fontSize: "8px" }}>
+                  Today, 10.40 am
+                </Typography>
+              </Box>
+            </Stack>
+            <Stack direction="row" sx={{ py: 1 }}>
+              <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+                NGN ⇌ USDT
+              </Typography>
+              <Box sx={{ ml: "auto" }}>
+                <Typography
+                  variant="body2"
+                  sx={{ fontWeight: "bold", color: "green" }}
+                >
+                  -$378.33
+                </Typography>
+                <Typography sx={{ fontSize: "8px" }}>
+                  Today, 10.40 am
+                </Typography>
+              </Box>
+            </Stack>
+            <Stack direction="row" sx={{ py: 1 }}>
+              <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+                NGN ⇌ USDT
+              </Typography>
+              <Box sx={{ ml: "auto" }}>
+                <Typography
+                  variant="body2"
+                  sx={{ fontWeight: "bold", color: "red" }}
+                >
+                  -$378.33
+                </Typography>
+                <Typography sx={{ fontSize: "8px" }}>
+                  Today, 10.40 am
+                </Typography>
+              </Box>
+            </Stack>
+          </Stack>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+};
+
+export default DbColumn3;
