5d19c4e6e4c4cb5c15901bfa8ad240333c8f976d
Xelem <anselmchizurum@gmail.com>
2023-03-21 20:36:08 +0100

    Modified the dashboard menu

--- src/components/DashboardMenu.jsx
+++ src/components/DashboardMenu.jsx
@@ -1,56 +1,15 @@
-import { styled } from "@mui/material/styles";
 import {
   AppBar,
   Toolbar,
   Typography,
-  InputBase,
   Stack,
   Avatar,
   IconButton,
 } from "@mui/material";
 
-import SearchIcon from "@mui/icons-material/Search";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import SettingsIcon from "@mui/icons-material/Settings";
 import LogoutIcon from "@mui/icons-material/Logout";
-import MenuIcon from "@mui/icons-material/Menu";
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: "#ffffff",
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(3),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
-}));
 
 const DashboardMenu = ({ openDrawer, setOpenDrawer }) => {
   return (
@@ -68,33 +27,43 @@ const DashboardMenu = ({ openDrawer, setOpenDrawer }) => {
     >
       <Toolbar sx={{ display: "flex" }}>
         <IconButton
-          onClick={() => setOpenDrawer(!openDrawer)}
-          size="large"
-          sx={{ display: { xs: "inline", md: "none" } }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography
-          variant="h6"
           sx={{
-            fontWeight: "bold",
-            py: 2,
-            px: 1,
+            display: {
+              xs: "inline",
+              md: "none",
+            },
           }}
         >
-          Welcome back, Tolu
-        </Typography>
-        <Stack direction="row" sx={{ ml: "auto" }}>
-          <Search sx={{ mr: 5 }}>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ "aria-label": "search" }}
-            />
-          </Search>
-
+          <Avatar
+            alt="User"
+            src="user.png"
+            sx={{ height: "25px", width: "25px" }}
+          />
+        </IconButton>
+        <Stack direction="column" sx={{ ml: 1 }}>
+          <Typography
+            variant="h6"
+            sx={{
+              fontWeight: "bold",
+              pt: 2,
+            }}
+          >
+            Hi, Tolu
+          </Typography>
+          <Typography sx={{ fontSize: "0.7rem" }}>
+            Tuesday, 7th March 2023
+          </Typography>
+        </Stack>
+        <Stack
+          direction="row"
+          sx={{
+            ml: "auto",
+            display: {
+              xs: "none",
+              md: "flex",
+            },
+          }}
+        >
           <IconButton sx={{ color: "#000000" }}>
             <NotificationsIcon />
           </IconButton>
@@ -104,7 +73,7 @@ const DashboardMenu = ({ openDrawer, setOpenDrawer }) => {
           <IconButton>
             <Avatar
               alt="User"
-              src="tiktok.png"
+              src="user.png"
               sx={{ height: "25px", width: "25px" }}
             />
             <Typography sx={{ ml: 1, fontWeight: "bold", color: "#000000" }}>
@@ -115,6 +84,18 @@ const DashboardMenu = ({ openDrawer, setOpenDrawer }) => {
             <LogoutIcon />
           </IconButton>
         </Stack>
+        <IconButton
+          sx={{
+            color: "#000000",
+            ml: "auto",
+            display: {
+              xs: "flex",
+              md: "none",
+            },
+          }}
+        >
+          <NotificationsIcon />
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
