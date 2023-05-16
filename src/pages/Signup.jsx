178e2dd240f06363b42dde84e0ddbde1255bf17e
Xelem <anselmchizurum@gmail.com>
2023-05-16 08:51:29 +0100

    Mobile view for signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,20 +1,15 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 import {
   Typography,
-  TextField,
-  InputAdornment,
   Button,
-  CircularProgress,
   Box,
-  FormHelperText,
   useMediaQuery,
   useTheme,
   Toolbar,
 } from "@mui/material";
 
-import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import AppleIcon from "@mui/icons-material/Apple";
 import EmailIcon from "@mui/icons-material/Email";
 
@@ -24,13 +19,6 @@ import { googleLogin } from "../utils/AuthFn";
 import Navbar from "../components/Navbar";
 
 const Signup = () => {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const [togglePassword, setTogglePassword] = useState("password");
-  const [visibilityColor, setVisibilityColor] = useState("817e7e");
-
-  const { loading } = useContext(UtilsContext);
-  const { errorMsg } = useContext(UtilsContext);
   const { setErrorMsg } = useContext(UtilsContext);
   const { setLoading } = useContext(UtilsContext);
   const navigate = useNavigate();
@@ -232,90 +220,66 @@ const Signup = () => {
             Payonize Login
           </Typography>
 
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              handleClick.login(email, password);
+          <Button
+            className="buttonStyle"
+            variant="contained"
+            fullWidth
+            sx={{
+              py: 1.5,
+              px: 3,
+              mt: 1,
+              mb: 2,
+              fontWeight: "bold",
+              textTransform: "capitalize",
+              background: "#b4b2b2",
+              color: "#000000",
+              ":hover": {
+                background: "#b4b2b2",
+              },
+            }}
+            onClick={async () => {
+              await handleClick.googleLogin();
             }}
           >
-            <label>Email</label>
-            <TextField
-              type="email"
-              fullWidth
-              required
-              size="small"
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-              sx={{ mb: 2 }}
-            />
+            <Box display="flex" alignItems="center" justifyContent="center">
+              <img
+                src="google.png"
+                alt="Google"
+                style={{ width: "25px", height: "25px" }}
+              />
+              <span style={{ marginLeft: "8px" }}>Continue with Google</span>
+            </Box>
+          </Button>
 
-            <label>Password</label>
-            <TextField
-              type={togglePassword}
-              fullWidth
-              required
-              size="small"
-              sx={{ mt: 1 }}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment
-                    position="end"
-                    sx={{ cursor: "pointer" }}
-                    onClick={() => {
-                      if (togglePassword === "password") {
-                        setTogglePassword("text");
-                        setVisibilityColor("54adf3");
-                      } else {
-                        setTogglePassword("password");
-                        setVisibilityColor("817e7e");
-                      }
-                    }}
-                  >
-                    <VisibilityOffIcon sx={{ color: `#${visibilityColor}` }} />
-                  </InputAdornment>
-                ),
-              }}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <FormHelperText error sx={{ mb: 2 }}>
-              {errorMsg.passwordLogin}
-            </FormHelperText>
-
-            <Button
-              className="buttonStyle"
-              type="submit"
-              variant="contained"
-              fullWidth
-              disabled={loading}
-              sx={{
-                py: 1.5,
-                px: 3,
-                mt: 1,
-                mb: 1,
-                fontWeight: "bold",
-                textTransform: "capitalize",
-                color: "#000000",
-                background: "#54adf3",
-                ":hover": {
-                  background: "#54adf3",
-                },
-              }}
-            >
-              {loading ? (
-                <CircularProgress size={"1.5rem"} sx={{ color: "#ffffff" }} />
-              ) : (
-                "Login"
-              )}
-            </Button>
-          </form>
+          <Button
+            className="buttonStyle"
+            variant="contained"
+            fullWidth
+            sx={{
+              py: 1.5,
+              px: 3,
+              mt: 3,
+              mb: 2,
+              textTransform: "capitalize",
+              fontWeight: "bold",
+              background: "#b4b2b2",
+              color: "#000000",
+              ":hover": {
+                background: "#b4b2b2",
+              },
+            }}
+          >
+            <Box display="flex" alignItems="center" justifyContent="center">
+              <AppleIcon sx={{ mr: 1 }} /> <span>Continue with Apple</span>
+            </Box>
+          </Button>
 
           <Box
             display="flex"
             alignItems="center"
             justifyContent="space-between"
             sx={{
-              my: 2,
+              my: 3,
             }}
           >
             <Box
@@ -337,12 +301,15 @@ const Signup = () => {
 
           <Button
             className="buttonStyle"
+            type="submit"
             variant="contained"
             fullWidth
             sx={{
               py: 1.5,
               px: 3,
-              mt: 1,
+              mt: 2,
+              mb: 1,
+              fontWeight: "bold",
               textTransform: "capitalize",
               background: "#b4b2b2",
               color: "#000000",
@@ -350,38 +317,10 @@ const Signup = () => {
                 background: "#b4b2b2",
               },
             }}
-            onClick={async () => {
-              await handleClick.googleLogin();
-            }}
+            onClick={() => navigate("/signup/email")}
           >
             <Box display="flex" alignItems="center" justifyContent="center">
-              <img
-                src="google.png"
-                alt="Nigeria"
-                style={{ width: "25px", height: "25px" }}
-              />
-              <span style={{ marginLeft: "8px" }}>Continue with Google</span>
-            </Box>
-          </Button>
-
-          <Button
-            className="buttonStyle"
-            variant="contained"
-            fullWidth
-            sx={{
-              py: 1.5,
-              px: 3,
-              mt: 3,
-              textTransform: "capitalize",
-              background: "#b4b2b2",
-              color: "#000000",
-              ":hover": {
-                background: "#b4b2b2",
-              },
-            }}
-          >
-            <Box display="flex" alignItems="center" justifyContent="center">
-              <AppleIcon sx={{ mr: 1 }} /> <span>Continue with Apple</span>
+              <EmailIcon sx={{ mr: 1 }} /> <span>Continue with Email</span>
             </Box>
           </Button>
 
